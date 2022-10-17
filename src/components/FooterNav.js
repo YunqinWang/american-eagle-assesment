f61a8a3fcbb287e505487c4a77b3a867a10bb26f
Yunqin Wang <wyq0307qyw@gmail.com>
2022-10-17 13:39:19 -0400

    footer expand and collapse

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -1,87 +1,70 @@
 import React from 'react'
+import FooterNavItem from './FooterNavItem';
 
 
 function FooterNav() {
+
     //footer nav list
     const footerList =[
 		{FooterItem:{
 			name: "Shop",
             subLink:[
-                {name: "baby apparel",link:"#",},
-                {name: "baby gifts",link:"#",},
-                {name: "furniture",link:"#",},
-                {name: "bedding",link:"#",},
-                {name: "lighting",link:"#",},
-                {name: "sale",link:"#",},
+                {name: "baby apparel",link:"#"},
+                {name: "baby gifts",link:"#"},
+                {name: "furniture",link:"#"},
+                {name: "bedding",link:"#"},
+                {name: "lighting",link:"#"},
+                {name: "sale",link:"#"},
             ]
 		}},
 		{FooterItem:{
 			name: "Service",
             subLink:[
-                {name: "contact us",link:"#",},
-                {name: "order status",link:"#",},
-                {name: "shipping policy",link:"#",},
-                {name: "return policy",link:"#",},
-                {name: "my account",link:"#",},
-                {name: "faq",link:"#",},
+                {name: "contact us",link:"#"},
+                {name: "order status",link:"#"},
+                {name: "shipping policy",link:"#"},
+                {name: "return policy",link:"#"},
+                {name: "my account",link:"#"},
+                {name: "faq",link:"#"},
             ]
 		}},
 		{FooterItem:{
 			name: "About",
             subLink:[
-                {name: "our story",link:"#",},
-                {name: "baby registry",link:"#",},
-                {name: "store location",link:"#",},
-                {name: "reviews",link:"#",},
-                {name: "news",link:"#",},
-                {name: "subscribe",link:"#",},
+                {name: "our story",link:"#"},
+                {name: "baby registry",link:"#"},
+                {name: "store location",link:"#"},
+                {name: "reviews",link:"#"},
+                {name: "news",link:"#"},
+                {name: "subscribe",link:"#"},
             ]
 		}},
+        {FooterItem:{
+			name: "Contact",
+            address1: "4472 Lawn Avenue",
+            address2: "Western Springs, IL 60558",
+            phone: "1-708-245-1212",
+            hours: "Store hours",
+            link:"#",
+		}},
 	]
+    
+    
     let footerItems = footerList.map(item=>{
-        let innerLink = item.FooterItem.subLink.map(subItem=>{
-            return <li key={subItem.name} className="footer-list-col-item">
-                        <a href={subItem.link}>{subItem.name}</a>
-                    </li>
-        })
 		return(
 			<li className="footer-list-col" key = {item.FooterItem.name}>
-                <ul className="footer-list-col-title">{item.FooterItem.name}</ul>
-				{innerLink}
+                <FooterNavItem footerCol={item.FooterItem} />
 			</li>
 		)
 	})
     
-    //social links
-    const links=[
-        {social:{name:"facebook" ,link:"#",svg:require("../images/facebook.svg").default}},
-        {social:{name:"instagram" ,link:"#",svg:require("../images/instagram.svg").default}},
-        {social:{name:"tiktok" ,link:"#",svg:require("../images/tiktok.svg").default}},
-        {social:{name:"twitter" ,link:"#",svg:require("../images/twitter.svg").default}},
-    ]
-    let socialLink = links.map(item=>{
-        console.log(item.social.name);
-        return(
-            <a key={item.social.name}>
-                <img src={item.social.svg} alt={item.social.name}/>
-            </a>
-        )
-    })
+    
 
     return (
         <div className = "footer-nav">
             <ul className = "footer-nav-list">
                 {footerItems}
             </ul>
-            <div className = "footer-nav-contact">
-                <h5 className="footer-list-col-title">Contact</h5>
-                <p>4472 Lawn Avenue <br />Western Springs, IL 60558</p>
-                <p>1-708-245-1212</p>
-                <a href="#">Store Hours</a>
-                <div className="footer-social-links">
-                    {socialLink}
-                </div>
-            </div>
         </div>
     )
 }
---
+++ src/components/FooterNavItem.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react'
+
+function FooterNavItem({footerCol}) {
+    const [expand, setExpand] =useState(false)
+    const btnClass = "footer-mobile-expand ";
+
+    let expandClass = btnClass;
+    let collapseClass = btnClass;
+    let subLiClass = "footer-list-col-item ";
+    let contactSubClass = "footer-nav-contact ";
+    let innerLink ;
+
+    if(!expand) {
+        expandClass += "show-inline";
+        collapseClass += "hide";
+        subLiClass += "hide";
+        contactSubClass += "hide";
+    }
+    else {
+        expandClass += "hide"
+        collapseClass += "show-inline";
+        subLiClass += "show-block";
+        contactSubClass += "show-block";
+    }
+
+    //social links
+    const links=[
+        {social:{name:"facebook" ,link:"#",svg:require("../images/facebook.svg").default}},
+        {social:{name:"instagram" ,link:"#",svg:require("../images/instagram.svg").default}},
+        {social:{name:"tiktok" ,link:"#",svg:require("../images/tiktok.svg").default}},
+        {social:{name:"twitter" ,link:"#",svg:require("../images/twitter.svg").default}},
+    ]
+    let socialLink = links.map(item=>{
+        console.log(item.social.name);
+        return(
+            <a key={item.social.name}>
+                <img src={item.social.svg} alt={item.social.name}/>
+            </a>
+        )
+    })
+    
+    if(footerCol.name!="Contact"){
+    innerLink = footerCol.subLink.map(subItem=>{
+            return (
+                <li key={subItem.name} className={subLiClass}>
+                    <a href={subItem.link}>{subItem.name}</a>
+                </li>
+            )
+        })
+    }
+    else{
+        innerLink = (
+            <div className = {contactSubClass}>
+                <p>{footerCol.address1} <br />{footerCol.address2}</p>
+                <p>{footerCol.phone}</p>
+                <a href={footerCol.link}>{footerCol.hours}</a>
+                <div className="footer-social-links">
+                    {socialLink}
+                </div>
+            </div>
+        )
+    }
+
+  return (
+    <div className="footer-list-col">
+            <ul className="footer-list-col-title">
+                {footerCol.name} 
+                <button onClick={()=>setExpand(!expand)} className={expandClass}>+</button> 
+                <button onClick={()=>setExpand(!expand)} className={collapseClass}>-</button>
+            </ul>
+            {innerLink}
+    </div>
+  )
+}
+
+export default FooterNavItem
